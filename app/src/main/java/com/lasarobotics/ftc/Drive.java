83a312dbb85bea7c0ab6843dfe9f35cd24251d60
Oksisane <ehsanmasdar@gmail.com>
2015-06-06 15:27:58 -0500

    Update Library project

--- app/src/main/java/com/lasarobotics/ftc/Drive.java
+++ app/src/main/java/com/lasarobotics/ftc/Drive.java
@@ -1,7 +1,59 @@
 package com.lasarobotics.ftc;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 /**
- * Created by Ehsan on 6/4/2015.
+ * Drive Train Math
  */
 public class Drive {
+    /**
+     * Implements the Tank drive train
+     * @param left Left motor
+     * @param right Right motor
+     * @param leftValue Left motor target value
+     * @param rightValue Right motor target value
+     */
+    public static void tank(DcMotor left, DcMotor right, double leftValue,double rightValue ){
+        left.setPower((leftValue/127)*100);
+        right.setPower((rightValue/127)*100);
+    }
+
+    /**
+     * Implements the Arcade drive train with three axis and four motors.
+     * @param y The y-axis of the controller, forward/rev
+     * @param x The x-axis of the controller, strafe
+     * @param c The spin axis of the controller
+     * @param leftFront The motor on the front left
+     * @param rightFront The motor on the front right
+     * @param leftBack The motor on the back left
+     * @param rightBack The motor on the back right
+     */
+    public static void mecanumArcade(double y,double x, double c,DcMotor leftFront,DcMotor rightFront,DcMotor leftBack, DcMotor rightBack){
+        double leftFrontVal = y + x + c;
+        double rightFrontVal = y - x - c;
+        double leftBackVal  = y - x + c;
+        double rightBackVal  = y + x - c;
+        leftFront.setPower(leftFrontVal*100);
+        rightFront.setPower(rightFrontVal*100);
+        leftBack.setPower(leftBackVal*100);
+        rightBack.setPower(rightBackVal*100);
+    }
+
+    /**
+     * Implements the Arcade drive train with field orientation
+     * @param y The y-axis of the controller, forward/rev
+     * @param x The x-axis of the controller, strafe
+     * @param c The spin axis of the controller
+     * @param gyroheading The current normalized gyro heading (between 0 and 360)
+     * @param leftFront The motor on the front left
+     * @param rightFront The motor on the front right
+     * @param leftBack The motor on the back left
+     * @param rightBack The motor on the back right
+     */
+    public static void mecanumArcadeFieldOriented(double y,double x, double c, double gyroheading,DcMotor leftFront,DcMotor rightFront,DcMotor leftBack, DcMotor rightBack){
+        double cosA = java.lang.Math.cos(java.lang.Math.toRadians(Util.normalizeGyro(gyroheading)));
+        double sinA = java.lang.Math.sin(java.lang.Math.toRadians(Util.normalizeGyro(gyroheading)));
+        double xOut = x * cosA - y * sinA;
+        double yOut = x * sinA + y * cosA;
+        mecanumArcade(yOut,xOut,c,leftFront,rightFront,leftBack,rightBack);
+    }
 }
---
+++ app/src/main/java/com/lasarobotics/ftc/Util.java
@@ -0,0 +1,85 @@
+package com.lasarobotics.ftc;
+
+/**
+ *  Mathematical and Precision Utilities
+ */
+public class Util {
+    /**
+     * Double equality epsilon
+     */
+    private final static double EPSILON = 0.000001;
+
+    /**
+     * Gives a "deadzone" where any value less
+     * than this would return zero.
+     *
+     * @param deadband Maximum value that returns zero
+     * @param value Value to test
+     * @return Deadbanded value
+     */
+    public static double deadband(double deadband, double value)
+    {
+        if (Math.abs(value) > deadband) { return value; }
+        return 0;
+    }
+
+    /**
+     * Returns if two double values are equal to within epsilon.
+     * @param a First value
+     * @param b Second value
+     * @return True if the values are equal, false otherwise
+     */
+    public static Boolean equal(double a, double b)
+    {
+        return (Math.abs(a - b) < EPSILON);
+    }
+
+    /**
+     * Returns if two double values are equal to within a distance.
+     * @param a First value
+     * @param b Second value
+     * @param distance Maximum distance between a and b
+     * @return True if the values are equal ot within distance, false otherwise
+     */
+    public static Boolean equal(double a, double b, double distance)
+    {
+        return (Math.abs(a - b) < distance);
+    }
+
+    /**
+     * Ignores values equal to the fail value (normally zero).
+     * @param value Current value
+     * @param lastvalue Previous value
+     * @param fail Filter this value, normally zero
+     * @return Filtered value
+     */
+    public static double filter(double value, double lastvalue, double fail)
+    {
+        return (value == fail) ? lastvalue : value;
+    }
+
+    /**
+     * Forces a numerical value to be between a min
+     * and a max
+     *
+     * @param min If less than min, returns min
+     * @param max If greater than max, returns max
+     * @param value Value to test
+     * @return Coerced value
+     */
+    public static double coerce(double min, double max, double value) {
+        return (value > max) ? max : (value < min) ? min : value;
+    }
+
+    /**
+     * Normalize Gyroscope bounds to within 0 and 360
+     * @param gyro The current Gyroscope value
+     * @return The normalized Gyroscope value, between 0 and 360.
+     */
+    public static double normalizeGyro(double gyro) {
+        if (gyro < 0){
+            return (360 + gyro) % 360;
+        }
+        return gyro % 360;
+    }
+}
