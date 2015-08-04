e667f8ce2bbb77bc1c8104ad5bbcdcf4eb3d691b
Arthur Pachachura <pachachura.arthur@gmail.com>
2015-08-04 13:46:32 -0500

    Reverse right side motors

--- app/src/main/java/com/lasarobotics/ftc/skynet/Kalman.java
+++ app/src/main/java/com/lasarobotics/ftc/skynet/Kalman.java
@@ -2,6 +2,7 @@ package com.lasarobotics.ftc.skynet;
 
 /**
  * Kalman filter implementation
+ * Takes in multiple sensors' data to produce highly-accurate position and velocity vector fields
  */
 public final class Kalman {
 
