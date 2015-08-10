bfebff7fb44a2fcacefa1c10fda3bb84e8031ee4
Arthur Pachachura <pachachura.arthur@gmail.com>
2015-08-09 22:47:09 -0500

    Start camera creation

--- app/src/main/java/com/lasarobotics/ftc/sensor/android/Camera.java
+++ app/src/main/java/com/lasarobotics/ftc/sensor/android/Camera.java
@@ -1,8 +1,19 @@
 package com.lasarobotics.ftc.sensor.android;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+
 /**
  * Implements the Android camera
  */
 public class Camera {
-
+    public static boolean isHardwareAvailable(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
 }
