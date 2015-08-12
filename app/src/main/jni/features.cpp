371602ceadcf9692bb890c8c219ff54cdaa1ca3d
Arthur Pachachura <pachachura.arthur@gmail.com>
2015-08-11 19:12:34 -0500

    Write feature highlighter

--- app/src/main/jni/features.cpp
+++ app/src/main/jni/features.cpp
@@ -1,9 +1,8 @@
 #include <string.h>
 #include <jni.h>
-#include "opencv2/opencv.hpp"
-#include "opencv2/objdetect/objdetect.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/features2d/features2d.hpp>
 using namespace std;
 using namespace cv;
 
@@ -15,5 +14,20 @@ extern "C"
     JNIEXPORT jstring JNICALL Java_com_lasarobotics_ftc_camera_detection_Features_stringFromJNI
             (JNIEnv *env, jobject type) {
         return env->NewStringUTF("Hello from JNI");
+    }
+
+    JNIEXPORT void JNICALL Java_com_lasarobotics_ftc_camera_detection_Features_highlightFeatures(JNIEnv*, jobject, jlong addrGray, jlong addrRgba)
+    {
+        Mat& mGr  = *(Mat*)addrGray;
+        Mat& mRgb = *(Mat*)addrRgba;
+        vector<KeyPoint> v;
+
+        Ptr<FeatureDetector> detector = FastFeatureDetector::create(50);
+        detector->detect(mGr, v);
+        for( unsigned int i = 0; i < v.size(); i++ )
+        {
+            const KeyPoint& kp = v[i];
+            circle(mRgb, Point(kp.pt.x, kp.pt.y), 10, Scalar(255,0,0,255));
+        }
     }
 }
