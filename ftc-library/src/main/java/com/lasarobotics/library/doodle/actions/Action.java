aa1dbdd68ed07cff24fe2dd5b54bf30e804083b1
Ehsan Asdar <ehsanmasdar@gmail.com>
2015-09-01 22:02:27 -0500

    Merge 0.1.0 Alpha from stage

---
+++ ftc-library/src/main/java/com/lasarobotics/library/doodle/actions/Action.java
@@ -0,0 +1,22 @@
+package com.lasarobotics.library.doodle.actions;
+
+import com.lasarobotics.library.doodle.DoodleRunData;
+
+/**
+ * Defines a custom robot action
+ * These actions are stored in the same file as the instruction data
+ */
+public abstract class Action {
+    private String name;
+
+    protected Action(String name)
+    {
+        this.name = name;
+    }
+
+    //Runs a snippet of code
+    public abstract void run(DoodleRunData data);
+
+    //Force the super class to create a toString() method
+    public abstract String toString();
+}
