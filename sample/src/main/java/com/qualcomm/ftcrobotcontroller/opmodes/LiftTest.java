5caa47384169748bf537b3e88bab77ac3c297f9f
Arthur Pachachura <pachachura.arthur@gmail.com>
2015-09-30 17:32:12 -0500

    Prepare for MonkeyC full test

--- sample/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/LiftTest.java
+++ sample/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/LiftTest.java
@@ -2,9 +2,9 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.lasarobotics.library.controller.ButtonState;
 import com.lasarobotics.library.controller.Controller;
+import com.lasarobotics.library.drive.Tank;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Gamepad;
 
 /**
  * Created by arthur on 9/29/15.
@@ -13,11 +13,17 @@ public class LiftTest extends OpMode {
 
     DcMotor lift;
     DcMotor liftAngle;
+    DcMotor frontLeft, frontRight, backLeft, backRight;
     Controller one;
 
     public void init() {
         lift = hardwareMap.dcMotor.get("lift");
         liftAngle = hardwareMap.dcMotor.get("liftAngle");
+        frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        frontRight = hardwareMap.dcMotor.get("frontRight");
+        backLeft = hardwareMap.dcMotor.get("backLeft");
+        backRight = hardwareMap.dcMotor.get("backRight");
+
         one = new Controller(gamepad1);
     }
 
@@ -36,7 +42,20 @@ public class LiftTest extends OpMode {
             liftAngle.setPower(0);
         }
 
-        lift.setPower(one.left_stick_y);
+        if (one.y == ButtonState.PRESSED || one.y == ButtonState.HELD)
+        {
+            liftAngle.setPower(1);
+        }
+        else if(one.a == ButtonState.PRESSED || one.a == ButtonState.HELD)
+        {
+            liftAngle.setPower(-1);
+        }
+        else {
+            liftAngle.setPower(0);
+        }
+
+
+        Tank.motor4(frontLeft, frontRight, backLeft, backRight, one.left_stick_y * -1, one.right_stick_y);
     }
 
     public void stop() {
